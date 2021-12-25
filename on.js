da613d5afa499e25d381e99406ed26a1bfb84b8a
向山　翔 <hms0175km@gmail.com>
2021-12-25 15:52:28 +0900

    [Add]メソッドを使うのが一番いい

--- on.js
+++ on.js
@@ -2,5 +2,10 @@ let app = new Vue({
     el: "#app",
         data:{
         clicked: false
-    }
+    },
+    methods: {
+        checkClick: function () {
+            this.clicked = true;
+        }
+    },
 })
